f52398f2329d0a8b5c92bf3a807956b085cfe9b2
ajrlewis <hello@ajrlewis.com>
2024-11-12 12:55:16 +0000

    Added foreign_keys to user to advoid metadata distorting table joins

--- models/booking.py
+++ models/booking.py
@@ -16,4 +16,14 @@ class Booking(db.Model, ModelMixin):
     invoice_id = db.Column(db.Integer, db.ForeignKey("invoice.id"), nullable=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
-    user = db.relationship("User", backref="booking")
+    user = db.relationship("User", backref="booking", foreign_keys=[user_id])
+
+    def to_dict(self) -> dict:
+        return {
+            "booking_id": self.id,
+            "date": f"{self.date}",
+            "time": f"{self.time}",
+            "customer_name": self.customer.name,
+            "service_name": self.service.name,
+            "user_name": self.user.name,
+        }
